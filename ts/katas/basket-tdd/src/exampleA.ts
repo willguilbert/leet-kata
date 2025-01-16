2b735f63f59eabe8e2de82233774adcf793b6c83
wguilbert <williamguilbert@hotmail.com>
2025-01-15 22:20:43 -0500

    chore(): added first kata

---
+++ ts/katas/basket-tdd/src/exampleA.ts
@@ -0,0 +1,19 @@
+export class ShoppingBasket {
+  add(item: BasketItem, quantity: number): void {
+    throw new Error("Not implemented");
+  }
+
+  get_quantity(item: BasketItem): number {
+    throw new Error("Not implemented");
+  }
+
+  calculate_total(): number {
+    throw new Error("Not implemented");
+  }
+}
+
+export class BasketItem {
+  constructor(name: string, price: number) {
+    throw new Error("Not Implemented");
+  }
+}
---
+++ ts/katas/basket-tdd/tests/exampleA.spec.ts
@@ -0,0 +1,20 @@
+import { BasketItem, ShoppingBasket } from "../src/exampleA";
+
+describe("ShoppingBasket", () => {
+  it("should instanciate a basket item with a name and a qty", () => {
+    const basket = new BasketItem("B", 20);
+    expect(basket);
+  });
+
+  it.skip("should calculate total with discount", () => {
+    const basket = new ShoppingBasket();
+    let item_a = new BasketItem("A", 10);
+    let item_b = new BasketItem("B", 25);
+    let item_c = new BasketItem("C", 9.99);
+    basket.add(item_a, 5);
+    basket.add(item_b, 2);
+    basket.add(item_c, 6);
+    expect(basket.get_quantity("C")).toBe(6);
+    expect(basket.calculate_total()).toBeCloseTo(151.94, 2);
+  });
+});
